259b76f96ec82e14f2008e0d37441b55d54f29ec
jwee <jwee@jweeui-MacBookAir.local>
2023-10-26 18:06:45 +0900

    fix : init file fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+  ],
+})
+
 export class AppModule {}
---
+++ backend/src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'ts',
+  password: 'ts',
+  database: 'ts',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};

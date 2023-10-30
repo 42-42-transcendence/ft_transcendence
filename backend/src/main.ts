b984daede8f9238181ad236da3a979671c0f5c37
Kim doo hee <doo3721@gmail.com>
2023-10-30 17:59:20 +0900

    FEAT: add swagger module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ft_transcendence API')
+    .setDescription('ft_transcendence 개발을 위한 API 문서입니다.')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

5ee914704da0875d431a5c59904ad111c1adacdf
한상민 <hansangmin@hansangmin-ui-MacBookAir.local>
2023-11-02 14:53:38 +0900

    FEAT: GPU adapter, device 초기화

--- front/src/components/WebGPU/Canvas.js
+++ front/src/components/WebGPU/Canvas.js
@@ -1,7 +1,21 @@
 export const initWebGPU = async (canvas) => {
-    // WebGPU 지원 여부 확인
+    /* 브라우저에서 WebGPU를 사용할 수 있는지 확인 */
     if (!navigator.gpu) {
         console.warn('WebGPU not supported');
-        return;
+        return null;
     }
+    /* adapter를 얻는 작업 */
+	/* navigator.gpu.requestAdapter()이 promise를 return해서 await로 대기 */
+	const adapter = await navigator.gpu.requestAdapter();
+	if (!adapter) {
+		throw new Error("No appropriate GPUAdapter found.");
+	}
+    
+    /* device(추상화된 GPU 객체)를 얻음 */
+	const device = await adapter.requestDevice();
+	if (!device) {
+		throw new Error("No appropriate GPUdevice found.");
+	}
+    
+    return { gpuDevice: device , gpuAdapter: adapter};
 };
--- front/src/routes/Game.tsx
+++ front/src/routes/Game.tsx
@@ -1,15 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { initWebGPU } from '../components/WebGPU/Canvas';
 
 const GamePage = () => {
+    /* useRef를 사용하여 canvas DOM을 가져옴 */
     const canvasRef = useRef(null);
+    /* useState를 사용하여 창의 높이를 가져옴 */
+    const [height, setHeight] = useState(window.innerHeight);
+
+    /* useEffect를 사용하여 창의 크기 변화를 감지 */
+    useEffect(() => {
+        /* 창의 크기가 변화할 때마다 높이를 가져옴 */
+        const handleResize = () => {
+            setHeight(window.innerHeight);
+        };
+        /* 창의 크기가 변화할 때마다 handleResize 함수를 실행 */
+        window.addEventListener('resize', handleResize);
+        handleResize(); // 초기 설정
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         if (!canvas) return;
 
         const init = async () => {
-            await initWebGPU(canvas);
+            try {
+                /* initWebGPU 함수를 실행하여 WebGPU를 초기화 */
+                const result = await initWebGPU(canvas);
+                if (result) {
+                    const { gpuDevice, gpuAdapter } = result;
+                } else {
+                    console.error("Failed to initialize WebGPU");
+                }
+            } catch (e) {
+                console.error("Failed to initialize WebGPU:", e);
+            }
         };
 
         init();
@@ -20,10 +46,11 @@ const GamePage = () => {
             <canvas
                 ref={canvasRef}
                 width="600"
-                height={window.innerHeight}
+                height={height}
                 style={{ backgroundColor: 'black', boxShadow: '0 4px 15px red' }}
             ></canvas>
         </main>
     );
 };
+
 export default GamePage;

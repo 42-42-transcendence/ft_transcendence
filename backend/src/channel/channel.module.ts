3985970c0e67b8b435cea93bcdea2107cb9d5a4e
doohee <doo3721@gmail.com>
2023-11-13 18:10:14 +0900

    FIX: channel 모듈에 JwtStrategy, PassportModule 추가

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -4,12 +4,14 @@ import { ChannelController } from './channel.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Channel } from './entities/channel.entity';
 import { ChannelRepository } from './channel.repository';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Channel])
   ],
   controllers: [ChannelController],
-  providers: [ChannelService, ChannelRepository],
+  providers: [ChannelService, ChannelRepository, JwtStrategy, PassportModule],
 })
 export class ChannelModule {}

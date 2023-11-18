953a5caacc1a3eec33f151914eafa7d92dac43b2
Kim doo hee <doo3721@gmail.com>
2023-11-18 14:21:26 +0900

    FIX: 모듈 의존성 수정, 삭제 파일 제거

--- backend/src/relation/relation.controller.ts
+++ backend/src/relation/relation.controller.ts
@@ -1,34 +1,8 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { RelationService } from './relation.service';
 import { RelationDto } from './dto/relation.dto';
-import { UpdateRelationDto } from './dto/update-relation.dto';
 
 @Controller('relation')
 export class RelationController {
   constructor(private readonly relationService: RelationService) {}
-
-  @Post()
-  create(@Body() RelationDto: RelationDto) {
-    return this.relationService.create(RelationDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.relationService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.relationService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRelationDto: UpdateRelationDto) {
-    return this.relationService.update(+id, updateRelationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.relationService.remove(+id);
-  }
 }
--- backend/src/relation/relation.module.ts
+++ backend/src/relation/relation.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RelationService } from './relation.service';
 import { RelationController } from './relation.controller';
+import { RelationRepository } from './relation.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Relation } from './entities/relation.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Relation]),
+  ],
   controllers: [RelationController],
-  providers: [RelationService],
+  providers: [RelationService, RelationRepository],
 })
 export class RelationModule {}

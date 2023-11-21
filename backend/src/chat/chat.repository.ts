1ac6131cbd52c10e801e6bfd3f179a898cb5fdec
Kim doo hee <doo3721@gmail.com>
2023-11-21 19:04:31 +0900

    WIP: 더미데이터 만들기 createChatDummy 추가

--- backend/src/chat/chat.repository.ts
+++ backend/src/chat/chat.repository.ts
@@ -3,6 +3,11 @@ import { DataSource, Repository } from "typeorm";
 import { CreateChatMessageDto } from "./dto/create-chat-message.dto";
 import { Chat } from "./entities/chat.entity";
 
+import { User } from "src/user/entities/user.entity";
+import { faker } from "@faker-js/faker";
+import { ChatType } from "./enums/chat-type.enum";
+import { Channel } from "src/channel/entities/channel.entity";
+
 @Injectable()
 export class ChatRepository extends Repository<Chat> {
 	constructor(private dataSource: DataSource) {
@@ -23,5 +28,18 @@ export class ChatRepository extends Repository<Chat> {
 		const result = await this.save(chat);
 
 		return (result);
+	}
+
+	async createChatDummy(channel: Channel, user: User) {
+
+		const dummy = this.create({
+			content: faker.lorem.lines(1),
+			chatType: ChatType.NORMAL,
+			userNickname: user.nickname,
+			channel,
+			user,
+		});
+
+		await this.save(dummy);
 	}
 }

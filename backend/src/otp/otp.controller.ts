34dcf0894ecdbfacc07b393310dfd135391d1da3
doohee <doo3721@gmail.com>
2023-12-04 19:40:19 +0900

    FEAT: otp 등록 및 확인 API 구현
    otp API 추가
    registerOtp 추가
    validateOtpCode 추가
    
    otp service 추가
    createOtpAuthUtl 추가
    validateOtpCode 추가
    pipeQrCodeStream 추가
    
    qrcode 모듈 추가

--- backend/src/otp/otp.controller.ts
+++ backend/src/otp/otp.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Res, UseGuards } from '@nestjs/common';
 import { OtpService } from './otp.service';
+import { UserService } from 'src/user/user.service';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { GetAuth } from 'src/auth/get-auth.decorator';
+import { Auth } from 'src/auth/entities/auth.entity';
+import { Response } from 'express';
 
-@Controller('otp')
+@ApiTags('OTP')
+@Controller('api/otp')
+@UseGuards(AuthGuard())
 export class OtpController {
-  constructor(private readonly otpService: OtpService) {}
+  constructor(
+    private otpService: OtpService,
+    private userService: UserService,
+  ) {}
+
+  // 일단 저장해놨다가 클라쪽에서 취소를 하면 지우는 식으로 가기
+  @Post()
+  async registerOtp(
+    @GetAuth() auth: Auth,
+    @Res() res: Response,
+  ) {
+    const user = await auth.user;
+    const secret = this.otpService.generateSecret();
+
+    await this.userService.saveOtpAuthSecret(user, secret);
+
+    const otpAuthUrl = this.otpService.createOtpAuthUtl(user);
+
+    return (this.otpService.pipeQrCodeStream(res, otpAuthUrl));
+  }
+
+  @Post('validate')
+  async validateOtpCode(
+    @GetAuth() auth: Auth,
+    @Body('code') code: string,
+  ): Promise<{ isValidate: boolean }> {
+    const user = await auth.user;
+    const isValidate = this.otpService.validateOtpCode(code, user);
+    
+    return ({ isValidate });
+  }
+
 }
--- backend/src/otp/otp.module.ts
+++ backend/src/otp/otp.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
 import { OtpController } from './otp.controller';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    UserModule,
+    AuthModule,
+  ],
   controllers: [OtpController],
   providers: [OtpService],
 })

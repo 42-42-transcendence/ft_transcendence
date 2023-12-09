31579f28c04bea591a163a00f4c2aeaf2e8b56f7
hansangmin <ghtrm1037@gmail.com>
2023-12-09 14:33:36 +0900

    REFACT: 테스트 환경 변경

--- front/src/components/GameModeForm/index.tsx
+++ front/src/components/GameModeForm/index.tsx
@@ -3,93 +3,36 @@ import styles from '../../styles/GameSelect.module.css';
 import GameModeSelectionList from './GameModeSelectionList';
 import CardButton from '../../UI/CardButton';
 import GameMatchingModal from '../Modal/GameMatchingModal';
-import {useState} from "react";
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import useModalState from '../../store/Modal/useModalState';
 import useOpenModal from '../../store/Modal/useOpenModal';
-import { useSocket } from '../../socket/SocketContext';
-import { io } from 'socket.io-client';
 
 const GameModeForm = () => {
   const showGameMatching = useModalState('showGameMatching');
-  const openHandler = useOpenModal('showGameMatching'); // submit handler
+  const openHandler = useOpenModal('showGameMatching');
   const [enteredMode, setEnteredMode] = useState<string>('normal');
-  const { socket } = useSocket();
-
-  const navigate = useNavigate();
-  const handlePlayerButton = async (playerType: 'player1' | 'player2') => {
-    const socket = io('http://localhost:3001/');
-
-    if (playerType === 'player1'){
-      if (socket) {
-        socket.emit('start', { gameMode: enteredMode });
-        console.log("emit start1");
-      }
-    }
-    if (playerType === 'player2'){
-      if (socket) {
-        socket.emit('start', { gameMode: enteredMode });
-        console.log("emit start2");
-      }
-    }
-    socket.on('created', () => {
-      console.log("game created by player");
-      // navigate(`/game/${data.gameId}`, { state: { gameMode: enteredMode, gameId: data.gameId, player: data.player } });
-    });
-    socket.on('joined', (data: any) => {
-      console.log("game joined by player");
-      openHandler();
-      navigate(`/game/${data.gameId}`, { state: { socket: socket, gameMode: enteredMode, gameId: data.gameId, player: [data.player1, data.player2]} });
-    });
-  }
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    const socket = io('http://localhost:3001/');
-    if (socket) {
-      socket.emit('create', { gameMode: enteredMode });
-      console.log("socket emit");
-      socket.on('created', () => {
-        console.log("hello world!!!!");
-        // navigate(`/game/${data.gameId}`, { state: { gameMode: enteredMode, gameId: data.gameId, player: data.player } });
-        });
-      }
+    e.preventDefault();
 
     openHandler();
-    // 1초 지연 (해당 부분에서, API 요청)
-    // socket.emit('create', { gameMode: enteredMode });
-    console.log('1초 지연');
-    await new Promise((res) => setTimeout(res, 1000));
-    // 받은 데이터 처리
-    navigate(`/game/` + "asdfasdf", { state: { gameMode: enteredMode, player: ["김봉삼", "감미선"]} });
   };
 
   return (
-    <>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '20px'}}>
-        <div>
-          <CardButton clickHandler={() => handlePlayerButton('player1')}>
-            Player 1
-          </CardButton>
-        </div>
-        <div>
-          <CardButton clickHandler={() => handlePlayerButton('player2')}>
-            Player 2
-          </CardButton>
-        </div>
-      </div>
+      <>
+        <Form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.container}>
+            <h2>GAME MODE</h2>
+            <GameModeSelectionList
+                setEnteredMode={setEnteredMode}
+                enteredMode={enteredMode}
+            />
+            <CardButton className={styles.start}>START</CardButton>
+          </div>
+        </Form>
 
-      <Form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.container}>
-          <h2>GAME MODE</h2>
-          <GameModeSelectionList setEnteredMode={setEnteredMode} enteredMode={enteredMode}/>
-          <CardButton className={styles.start} clickHandler={openHandler}>
-            START
-          </CardButton>
-        </div>
-      </Form>
-
-      {showGameMatching && <GameMatchingModal mode={''} />}
-    </>
+        {showGameMatching && <GameMatchingModal mode={enteredMode} />}
+      </>
   );
 };
 

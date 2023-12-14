d22af74fcb8b031f5cf0df6865be18041a28fd01
malbong <gon3578@gmail.com>
2023-12-14 14:09:53 +0900

    FIX: 프로필 페이지 유저 아이디 체크 수정

--- front/src/routes/SettingProfile.tsx
+++ front/src/routes/SettingProfile.tsx
@@ -1,15 +1,9 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import SettingProfileForm from '../components/SettingProfile/SettingProfileForm';
-import useUserState from '../store/User/useUserState';
 
 const SettingProfilePage = () => {
   const location = useLocation();
   const jwtToken = location.state?.jwtToken;
-  const userState = useUserState();
-
-  if (userState.id !== '') {
-    return <Navigate to="/" replace={true} />;
-  }
 
   if (jwtToken === null) {
     return <Navigate to="/login" replace={true} />;
